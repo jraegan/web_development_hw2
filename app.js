28fc556ee64c387e5138afd358142909c80cfebc
jraegan <116770830+jraegan@users.noreply.github.com>
2023-03-16 19:40:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function initMap(){
   var options = {
-    center: { lat: 31.8026, lng: 85.9572 };
+    center: { lat: 31.8026, lng: 85.9572 },
       zoom: 8
   }
   map = new google.maps.Map(document.getElementById("map"), options)
